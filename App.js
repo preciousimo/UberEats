4a283377fff419a958630ff8d086e73d83781143
preciousimo <preciousimoniakemu@gmail.com>
2022-09-22 12:50:00 +0100

    Menu item detailed screen

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
 import RestaurantDetailsScreen from "./src/screens/RestaurantDetailsScreen";
+import DishDetailsScreen from "./src/screens/DishDetailsScreen";
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <RestaurantDetailsScreen />
+      {/* <RestaurantDetailsScreen /> */}
       {/* <HomeScreen /> */}
+      <DishDetailsScreen />
       <StatusBar style="light" />
     </View>
   );
---
+++ src/screens/DishDetailsScreen/index.js
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+import restaurants from "../../../assets/data/restaurants.json";
+const dish = restaurants[0].dishes[0];
+
+const DishDetailsScreen = () => {
+  const [quantity, setQuantity] = useState(1);
+
+  const onMinus = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const onPlus = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const getTotal = () => {
+    return (dish.price * quantity).toFixed(2);
+  }
+
+  return (
+    <View style={styles.page}>
+      <Text style={styles.name}>{dish.name}</Text>
+      <Text style={styles.description}>{dish.description}</Text>
+      <View style={styles.seperator} />
+
+      <View style={styles.row}>
+        <AntDesign
+          name="minuscircleo"
+          size={60}
+          color={"black"}
+          onPress={onMinus}
+        />
+        <Text style={styles.quantity}>{quantity}</Text>
+        <AntDesign
+          name="pluscircleo"
+          size={60}
+          color={"black"}
+          onPress={onPlus}
+        />
+      </View>
+
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>Add {quantity} to basket (₦{getTotal()})</Text>
+      </View>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    width: "100%",
+    paddingVertical: 40, //temporary fix
+    padding: 10,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  description: {
+    color: "gray",
+  },
+  seperator: {
+    height: 1,
+    backgroundColor: "lightgrey",
+    marginVertical: 10,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 50,
+  },
+  quantity: {
+    fontSize: 25,
+    marginHorizontal: 20,
+  },
+  button: {
+    backgroundColor: "black",
+    marginTop: "auto",
+    padding: 20,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 18,
+  },
+});
+
+export default DishDetailsScreen;
